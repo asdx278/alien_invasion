79358d21e57058e5510f0bfd9f3b7e50f6eddcf3
Albert S <albert.shubin@gmail.com>
2025-08-18 23:26:27 +0400

    add bg_color, tick

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,9 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        # Фоновый цвет
+        self.bg_color = (230, 230, 230)
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
@@ -17,9 +20,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # При каждом проходе цикла перерисовать экран
+            self.screen.fill(self.bg_color)
 
 
             pygame.display.flip()
+            # Частота кадров
+            self.clock.tick(60)
             """Отображение последнего прорисованного экрана"""
 
 if __name__ == '__main__':
